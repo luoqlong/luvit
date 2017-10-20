7b51363fe98b9f1fdfc4641b7b247b8ddc6b10d2
luoqlong <361129147@qq.com>
2017-10-20 17:29:53 +0800

    luvit

---
+++ make.bat
@@ -0,0 +1,34 @@
+@ECHO off
+@SET LIT_VERSION=3.5.4
+
+IF NOT "x%1" == "x" GOTO :%1
+
+:luvit
+IF NOT EXIST lit.exe CALL make.bat lit
+ECHO "Building luvit"
+lit.exe make
+if %errorlevel% neq 0 goto error
+GOTO :end
+
+:lit
+ECHO "Building lit"
+PowerShell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://github.com/luvit/lit/raw/%LIT_VERSION%/get-lit.ps1'))"
+GOTO :end
+
+:test
+IF NOT EXIST luvit.exe CALL make.bat luvit
+ECHO "Testing luvit"
+SET LUVI_APP=.
+luvit.exe tests\run.lua
+if %errorlevel% neq 0 goto error
+SET "LUVI_APP="
+GOTO :end
+
+:clean
+IF EXIST luvit.exe DEL /F /Q luvit.exe
+IF EXIST lit.exe DEL /F /Q lit.exe
+
+:error
+exit /b %errorlevel%
+
+:end
